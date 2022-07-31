3552746347a5218ada14667eae6d75378271566a
Rajeev Rawal <rajivmum@gmail.com>
2022-07-31 14:10:25 +0800

    Code sample - FOR Loop example

--- Class1/FirstPython.py
+++ Class1/FirstPython.py
@@ -1,4 +1,11 @@
 print("Hello","world", end=" "); print('Bye','world', sep="#");
 
+message="My first python string"
+print(message)
+print("==============For Loop Demo==============")
+for x in range(6):
+    print(message)
+print ("==============for lopp end==============")
+
 def multiply(x,y): 
     return x*y;
