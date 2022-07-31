db8b5ae978f0849dc17d677448c4e6f4647110aa
Rajeev Rawal <rajivmum@gmail.com>
2022-07-31 15:04:00 +0800

    Working with different datatyp in python

---
+++ Class1/PythonDataType.py
@@ -0,0 +1,27 @@
+#Different data type in python
+from calendar import prmonth
+
+
+print('-----------------------------------------------------------------------------------------')
+#int datatype
+x = 9223372036854775807
+print('Value of x:', x)
+print('Type of x:', type(x))
+
+
+print('-----------------------------------------------------------------------------------------')
+#String datatype
+message = 'Hello, world!'
+print('Value of message:', message)
+print('Type of message:', type(message))
+print('-----------------------------------------------------------------------------------------')
+#Float datatype
+price = 9.999999999
+print('Value of price:', price)
+print('Type of price:', type(price))
+print('-----------------------------------------------------------------------------------------')
+#boolean datatype
+isPythonEasy = True
+print('Value of isPythonEasy:', isPythonEasy)
+print('Type of isPythonEasy:', type(isPythonEasy))
+print('-----------------------------------------------------------------------------------------')
