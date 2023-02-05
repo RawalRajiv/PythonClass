645b078f140642b1ea2b0ccb52eda7b19fbbf5a7
Rajeev Rawal <rajivmum@gmail.com>
2023-02-05 19:42:37 +0800

    Added new file for TryCatch Example

---
+++ Class1/TryCatchExample.py
@@ -0,0 +1,21 @@
+while True:
+    userValue = input("Please enter raw value:");
+    try:
+        userInputValue = int(userValue)
+        if(userInputValue <=0):
+            raise ValueError
+        elif userInputValue >= 91:
+            print("A+ Grade")
+        elif userInputValue < 91 and userInputValue >= 75:
+            print("A Grade")
+        elif userInputValue < 75 and userInputValue >= 60:
+            print("B Grade")
+        elif userInputValue < 60 and userInputValue >=50:
+            print("C Grade")
+        elif userInputValue < 50 and userInputValue >= 35:
+            print("D Grade")
+        else:
+            print("F Grade")
+        break
+    except ValueError:
+        print("Kindly key in proper value")
